c499218e9051347a5283b289958db18f63ca43a6
1roo <93hanaru@gmail.com>
2023-05-18 15:35:55 +0900

    202305 결혼정보업체 관리시스템

---
+++ matchMaking/src/com/marriage/main/Main.java
@@ -0,0 +1,24 @@
+package com.marriage.main;
+
+import static com.marriage.view.AppUI.*;
+
+public class Main {	
+
+	public static void main(String[] args) {
+		
+		AppController appController = new AppController();
+		showDraw();
+		while(true) {
+			showMainMenu();
+			int selectNum = inputInteger();
+			try {
+			appController.selectMainMenu(selectNum);
+			} catch(NullPointerException e) {
+				
+			}
+		}
+		
+
+	}
+
+}
